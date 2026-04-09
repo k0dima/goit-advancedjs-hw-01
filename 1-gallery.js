1c6252bf87394c0032a0d4920630c21b132c98be
k0dima <214964089+k0dima@users.noreply.github.com>
2026-04-09 07:39:11 +0000

    Deploying to gh-pages from @ k0dima/goit-advancedjs-hw-01@9a9f92b56fa0d3b452ca3841b938e0f86dcda171 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,12 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Gallery Page");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as r}from"./assets/vendor-CgTBfC_f.js";const i=[{largeImg:"./img/gallery/large/1.jpg",smallImg:"./img/gallery/small/1.jpg",alt:"Team brainstorming and planning with sticky notes"},{largeImg:"./img/gallery/large/1-1.jpg",smallImg:"./img/gallery/small/1-1.jpg",alt:"Modern meeting room with workspace setup"},{largeImg:"./img/gallery/large/1-2.jpg",smallImg:"./img/gallery/small/1-2.jpg",alt:"Laptop workspace with coffee and accessories"},{largeImg:"./img/gallery/large/2.jpg",smallImg:"./img/gallery/small/2.jpg",alt:"Creative idea represented by a glowing light bulb"},{largeImg:"./img/gallery/large/2-1.jpg",smallImg:"./img/gallery/small/2-1.jpg",alt:"Colorful creative office interior"},{largeImg:"./img/gallery/large/2-2.jpg",smallImg:"./img/gallery/small/2-2.jpg",alt:"Person working on laptop"},{largeImg:"./img/gallery/large/3.jpg",smallImg:"./img/gallery/small/3.jpg",alt:"Workspace — team planning session"},{largeImg:"./img/gallery/large/3-1.jpg",smallImg:"./img/gallery/small/3-1.jpg",alt:"Workspace — modern meeting room"},{largeImg:"./img/gallery/large/3-2.jpg",smallImg:"./img/gallery/small/3-2.jpg",alt:"Workspace — laptop desk setup"}],s=({largeImg:g,smallImg:e,alt:m})=>`
+<li class="gallery-item">
+	<a class="gallery-link" href="${g}">
+		<img
+			class="gallery-image"
+			src="${e}"
+			alt="${m}"
+			/>
+	</a>
+</li>
+`,a=i.map(s).join("");console.log(a);const l=document.createElement("ul");l.classList.add("gallery");l.insertAdjacentHTML("beforeend",a);const t=document.querySelector(".container h1");t.after(l);new r(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
